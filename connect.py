ca6517972b0d4e72a00f2bb5914d8018a7976241
Joshua Collister <55206396+jcollister@users.noreply.github.com>
2021-11-10 22:51:55 -0500

    Add files via upload

--- connect.py
+++ connect.py
@@ -1,7 +1,9 @@
 import socket
 from binascii import unhexlify, hexlify
-from itertools import cycle
 import base64
+from itertools import cycle
+from math import floor
+import math
 
 def GetInfo(Cipher):
     C = Cipher.split("\\n")[1].split("\\n")
@@ -10,21 +12,27 @@ def GetInfo(Cipher):
     IV = iv[1].replace("'","")
     return CipherText,IV
 
+def splitBlocks(message):
+    Size = len(message)
+    Slices = []
+    Increment =int(Size/32)
+    for i in range(Increment):
+        Slices.append(message[i*32:(i+1)*32])
+    print Slices
+    return Slices
+
 def xor(arg1,arg2):
-    return hexlify(''.join(chr(ord(arg1) ^ ord(arg2)) for arg1, arg2 in zip(unhexlify(arg1), cycle(unhexlify(arg2)))))
+    return hexlify(''.join(chr(ord(a) ^ ord(b)) for a, b in zip(unhexlify(arg1), unhexlify(arg2))))
+
 
 def requestServer(prefix):
     r.send(("-e " + str(prefix)).encode())     # Encryption of the secret message
     x = r.recv(1024).decode()
     return x
 def checkValidate(prefix):
-    r.send(("-v " + str(prefix)).encode())     # Encryption of the secret message
+    r.send(("-V " + str(prefix)).encode())     # Encryption of the secret message
     x = r.recv(1024).decode()
     return x
-
-
-
-
 
 
 r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,38 +43,34 @@ r.connect(("128.186.120.191", 31337))
 
 
 
-r.send("-e".encode())     # Encryption of the secret message
-x = r.recv(1024).decode()
-r.send("-e 00000000" .encode())     # Encryption of the secret message
-
+#r.send("-e".encode())     # Encryption of the secret message
+#x = r.recv(1024).decode()
+#r.send("-e".encode())
+#y = r.recv(1024).decode()
+x = requestServer('')
 Ciphertext, IV = GetInfo(x)
+#CT2, IV2 = GetInfo(y)
+CTencode = bytes(Ciphertext)
+#CT = bytes(CT2)
+c = xor(Ciphertext, IV)
+print(IV)
 print(Ciphertext)
-print(IV)
-CTencode = bytearray(Ciphertext, 'utf-8')
-IVencode = bytearray(IV, 'utf-8')
-print(x)
-print(CTencode)
-print(IV)
-print(len(CTencode)/2)
-print("Xor")
-print(xor(Ciphertext,Ciphertext))
-print(type(x))
-print(type(CTencode))
-print(type(IVencode))
-
+print(c)
+print(splitBlocks('8ba32609aa15bea4d686831382c8c33c7ghd7'))
 
 #r.send("-e abcdef0123456789".encode()) # Encryption of abcdef0123456789 || message
 #x = r.recv(1024).decode()
 #print x.replace(" ", "")
 
 
-#r.send("-V 6a9215b64a244b92b84dbe7cc58a5d02e8f029f432f8e931c8add86808118c5c8af29756f6d6ff85ae16ad77dc0b8221816c1dda9825f9a407b03d3f9a817160715f0b89abf9f1213bf45f464730b810 659a4a7cf4273befce277e5336202d69".encode()) # Valid ciphertext and IV
-#x = r.recv(1024).decode()
-#print x
+r.send("-V 6a9215b64a244b92b84dbe7cc58a5d02e8f029f432f8e931c8add86808118c5c8af29756f6d6ff85ae16ad77dc0b8221816c1dda9825f9a407b03d3f9a817160715f0b89abf9f1213bf45f464730b810 659a4a7cf4273befce277e5336202d69".encode()) # Valid ciphertext and IV
+z = r.recv(1024).decode()
+print z
 
+y = checkValidate('6a9215b64a244b92b84dbe7cc58a5d02e8f029f432f8e931c8add86808118c5c8af29756f6d6ff85ae16ad77dc0b8221816c1dda9825f9a407b03d3f9a817160715f0b89abf9f1213bf45f464730b810 659a4a7cf4273befce277e5336202d69')
+print y
 
-
-#r.send("-V 6a9215b64a244b92b84dbe7cc58a5d02e8f029f432f8e931c8add86808118c5c8af29756f6d6ff85ae16ad77dc0b8221816c1dda9825f9a407b03d3f9a817160715f0b89abf9f1213bf45f464730b810 659a4a7cf4273befce277e5336202d6a".encode()) # Invalid (flipped IV bit)
+#r.send("-V 6a96a9215b64a244b92b84dbe7cc58a5d02e8f029f432f8e931c8add86808118c5c8af29756f6d6ff85ae16ad77dc0b8221816c1dda9825f9a407b03d3f9a817160715f0b89abf9f1213bf45f464730b810 659a4a7cf4273befce277e5336202d69215b64a244b92b84dbe7cc58a5d02e8f029f432f8e931c8add86808118c5c8af29756f6d6ff85ae16ad77dc0b8221816c1dda9825f9a407b03d3f9a817160715f0b89abf9f1213bf45f464730b810 659a4a7cf4273befce277e5336202d6a".encode()) # Invalid (flipped IV bit)
 #x = r.recv(1024).decode()
 #print x
 
