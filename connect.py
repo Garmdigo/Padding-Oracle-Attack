88a5076966f46b8f8d134b76ef3ca3e5f3ccdc78
Joshua Collister <55206396+jcollister@users.noreply.github.com>
2021-11-12 18:02:30 -0500

    Add files via upload

--- connect.py
+++ connect.py
@@ -12,7 +12,13 @@ def GetInfo(Cipher):
     IV = iv[1].replace("'","")
     return CipherText,IV
 
-
+def splitBlocks(message):
+    Size = len(message)
+    Slices = []
+    Increment =int(Size/32)
+    for i in range(Increment):
+        Slices.append(message[i*32:(i+1)*32])
+    return Slices
 
 def sliceBlocks(message):
     size = len(message) /2
@@ -22,7 +28,7 @@ def sliceBlocks(message):
     y = 32
     v = size % 32
     if(v):
-        iter = floor(len(message)/32)
+        iter = len(message)/32
         tmp = []
         for i in range(iter):
            slices.append(message[x:y+1])
@@ -55,24 +61,23 @@ def sliceBlocks(message):
                 v = 0
     return slices
 
+def sliceBlock(message):
+    size = len(message)
+    slices = []
+    x = 0
+    y = 32
+    v = size % 32    
+    for i in range(size / 32):
+       slices.append(message[x:y])
+       x += 32
+       y += 32
+    if(v!=0):
+      slices.append(message[x:y])
+
+    return slices
+
 def xor(arg1,arg2):
-    print(arg1,arg2)
-    if( len(arg1) % 2 ==0 & len(arg2) % 2 ==0):
-        return hexlify(''.join(chr(ord(a) ^ ord(b)) for a, b in zip(unhexlify(arg1), cycle(unhexlify(arg2)))))
-    if (len(arg1) %2 !=0  & len(arg2) % 2 != 0):
-        arg1 = unhexlify('0%x' % arg1)
-        arg2 = unhexlify('0%x' % arg2)
-        return hexlify(''.join(chr(ord(a) ^ ord(b)) for a, b in zip(arg1, cycle(arg2))))
-
-    if ( len(arg2) %2 == 0 & len(arg1) %2 != 0):
-        arg1 = unhexlify('0%x' % n)
-        return hexlify(''.join(chr(ord(a) ^ ord(b)) for a, b in zip(arg1, cycle(unhexlify(arg2)))))
-    
-    if ( len(arg1) %2 == 0 & len(arg2) %2 != 0):
-        arg2 = unhexlify('0%x' % n)
-        return hexlify(''.join(chr(ord(a) ^ ord(b)) for a, b in zip(unhexlify(arg1), cycle(arg2))))
-
-  
+    return hexlify(''.join(chr(ord(a) ^ ord(b)) for a, b in zip(unhexlify(arg1), unhexlify(arg2))))
 
 
 def requestServer(prefix):
@@ -91,10 +96,76 @@ def listToString(input):
     return str
 
 def getNtoLast(bytes,position):
-    return listToString(bytes[-positon])
+    return bytes[-positon]
 def LastNbyte(bytes,getNtoLast,choosebyte):
     LastBlock = getNtoLast(bytes,getNtoLast)
-    return LastBlock[-choosebyte]
+    return LastBlock[-choosebyte:]
+
+def testOracle():
+     t = requestServer('')
+     Ciphertext, IV = GetInfo(t)
+     d = sliceBlock(Ciphertext)
+     b=d[0]
+     d[-1]=b
+     st = listToString(d)
+     z = checkValidate(str(st), str(IV))
+     return st, IV, z
+
+def prefixTest(C, Cprime):
+    B = sliceBlock(Cprime)
+    b = B[0]			#first block of Cprime
+    D = sliceBlock(C)
+    D[-1] = b			#swap c4 to b1
+    st = listToString(D)
+    return st
+
+def returnithBlock(ciphertext, i):
+   blocks = sliceBlock(ciphertext)
+   b = blocks[i]
+   return b
+
+def validDecrypt():
+    st, IV, z = testOracle()
+    if(z == "Valid"):
+	return st, IV, z
+    else:
+        while(z!="Valid"):
+           st, IV, z = testOracle()
+           if(z == "Valid"):
+                  block2 = returnithBlock(st, -2)
+                  result = xor(IV, block2)
+                  result2 = xor(result[-2:], '15')
+                  test = requestService("")
+                  newCT, newIV = GetInfo(test)
+                  print(st)
+                  print(IV)
+                  print(block2)
+                  print(result)
+                  print(result2)
+	          y = requestServer("00000000")
+                  C, I = GetInfo(y)
+                  B = prefixTest(st,C)
+                  val = checkValidate(str(B), str(IV))
+                  if(val == "Valid"):
+			print("WE'RE Close")
+                        return B, IV, val
+                  else:
+                    while(val!="Valid"):
+                      y = requestServer("00000000")
+                      C, I = GetInfo(y)
+                      B = prefixTest(st,C)
+                      val = checkValidate(str(B), str(IV))
+                      if(val == "Valid"):
+                        print("WE'RE Closer")
+                        #block = returnithBlock(B,0)
+                        block2 = returnithBlock(st, -2)
+                        result = xor(I, block2)
+                        result2 = xor(result[-2:], '15')
+                        return B, IV, val, result2
+
+              
+	         
+
 
 r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 r.connect(("128.186.120.191", 31337))
@@ -108,41 +179,52 @@ r.connect(("128.186.120.191", 31337))
 #x = r.recv(1024).decode()
 #r.send("-e".encode())
 #y = r.recv(1024).decode()
+
 x = requestServer('')
-
 Ciphertext, IV = GetInfo(x)
-#CT2, IV2 = GetInfo(y)
-#CTencode = bytes(Ciphertext)
-#CT = bytes(CT2)
-print("Ciphertext")
-print(Ciphertext)
-print("IV")
-print(IV)
-print("new line")
-d = sliceBlocks(Ciphertext)
-print(d)
-print("sasd")
+y = requestServer('00000000')
+CT2, IV2 = GetInfo(y)
+#B = prefixTest(Ciphertext, CT2)
+#print(Ciphertext)
+#print(CT2)
+#print(B)
 
-b=d[0]
-d[-1]=b
-a= d[-1]
-c = xor(d[0], IV)
-print(c)
-print("assdad")
-print(LastNbyte(c,1,1))
+#d = sliceBlock(Ciphertext)
+#b=d[0]
+#d[-1]=b
+#a= d[-1]
+#c = xor(d[0], IV)
+#print(c)
+#st = listToString(d)
+#CT, IV3, val = testOracle()
+CT, IV3, val, hexval = validDecrypt()
+#block = returnithBlock(CT,0)
+#block2 = returnithBlock(Ciphertext, -2)
+#result = xor(IV2, block2)
+#result2 = xor(result[-2:], '15')
+print(CT)
+print(IV3)
+print(val)
+print(hexval)
+#print(result)
+#print(result2)
+#print(IV3)
+#print(val)
+#y = requestServer("00000000")
+#C, I = GetInfo(y)
+#print(C,I)
 
-str = listToString(d)
 #print(str[0])
-#u = bytearray(str, 'latin-1')
-#nIV = bytearray(IV, 'latin-1')
-#print(type(u))
-#z = checkValidate(u, nIV)
+#u = str.decode()
+#nIV = IV.encode()
+#print(type(nIV))
+#z = checkValidate(str(st), str(IV))
 #print(z)
-print(a[-2])
-print(d)
-print(IV)
-print(Ciphertext)
-print(c)
+#print(a[-2])
+#print(d)
+#print(IV)
+#print(Ciphertext)
+#print(type(c))
 
 #r.send("-e abcdef0123456789".encode()) # Encryption of abcdef0123456789 || message
 #x = r.recv(1024).decode()
